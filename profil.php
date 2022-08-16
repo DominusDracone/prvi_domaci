41778559888f4ba36a145b65d58dcab987d89c77
Pavle Lazarevic <pavleslazarevi@gmail.com>
2022-08-16 19:47:00 +0200

    Ispravljanje sitnih grešaka

--- profil.php
+++ profil.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Forma</title>
-    <link href="css/styleForma.css" rel="stylesheet">
+    <title>Profil</title>
+    <link href="css/styleProfil.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body class="text-center">
@@ -12,7 +12,7 @@
     <main class="form-signin w-100 m-auto">
       <form>
         
-        <h1 class="h3 mb-3 fw-normal">Ko će nam se pridružiti?</h1>
+        <h1 class="h3 mb-3 fw-normal">Šta želiš od mene?</h1>
     
         <div class="fieldset-1">
             <label for="Slika">Slika</label>
@@ -69,7 +69,11 @@
         </div>
         
        
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Zapamti</button>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Zakaži sastanak</button>
+        <button class="w-100 btn btn-lg btn-primary btnIzb " type="submit">Obriši</button>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Izmeni</button>
+        <button class="w-100 btn btn-lg btn-primary btnNazad" type="submit">Nazad</button>
+        
       </form>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
